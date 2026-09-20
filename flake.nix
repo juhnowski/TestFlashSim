0b12ed4fb9e67210a4bac35565d5e17c4081d99d
Juhnowski <juhnowski@gmail.com>
2026-09-20 12:55:20 +0300

    test 4/5

--- flake.nix
+++ flake.nix
@@ -1,41 +1,44 @@
+# ~/TestFlashSim/OriginalFlashSim/flake.nix
 {
-  description = "Воспроизводимая среда разработки для FlashSim (C++)";
+  description = "Воспроизводимая среда разработки для FlashSim (C++) и эмуляции ZNS";
 
   inputs = {
-    # Используем стабильный срез пакетов nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }:
     let
-      # Указываем архитектуру вашего процессора (для ПК это x86_64-linux)
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        # Пакеты, которые будут доступны внутри среды разработки
         buildInputs = with pkgs; [
-          gcc        # Компилятор GNU C/C++ (включая g++)
-          gnumake    # Утилита Make для запуска файлов Makefile
-          cmake      # Дополнительный генератор сборок, если потребуется
-          git        # Утилита управления версиями
-	  boost
+          gcc        # Компилятор GNU C/C++
+          gnumake    # Утилита Make
+          cmake      # Генератор сборок
+          git        # Управление версиями
+          boost      # Библиотека Boost для FlashSim
+
+          # Инструменты для работы с ZNS и виртуализацией:
+          qemu       # Эмулятор для запуска ядра с поддержкой ZNS
+          qemu-utils # Утилита qemu-img для создания образов дисков
+          nvme-cli   # Утилита управления NVMe накопителями
+          wget       # Для скачивания Live-CD образов ядра
         ];
 
-        # Скрипт, который автоматически выполнится при входе в nix develop
         shellHook = ''
           export LANG=ru_RU.UTF-8
           echo ""
           echo "======================================================="
-          echo "🚀 Добро пожаловать во флейк-окружение для FlashSim!"
-          echo "Доступны: g++, make, git."
-          echo "Для сборки симулятора введите: make test"
+          echo "🚀 Добро пожаловать во флейк-окружение для FlashSim & ZNS!"
+          echo "Доступны: g++, make, boost, qemu, nvme-cli."
+          echo "Сборка FlashSim:   make test"
+          echo "Создать ZNS образ:  qemu-img create -f raw /tmp/zns.img 10G"
           echo "======================================================="
           echo ""
 
-          # Красивый кастомный prompt, чтобы вы видели, что находитесь внутри флейка
-          export PS1="[flashsim-flake:\w]\$ "
+          export PS1="[flashsim-zns-flake:\w]\$ "
         '';
       };
     };
